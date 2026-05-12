d3ca75c3f181f234e1e648018ff23f19747b8f9a
AlbertoOS23 <linkrojo13@gmail.com>
2026-05-12 12:12:26 +0200

    Crear tasca y estilos de las tarjetas funcional

--- js/script.js
+++ js/script.js
@@ -52,6 +52,92 @@ function pruebaApp() {
     } else {
         console.log("Tareas", tasques)
     }
+
+    renderTauler();
+}
+
+/* elementos del DOM con los que trabajaremos */
+
+const formulariTasca = document.getElementById("formulari-tasca");
+const idTasca = document.getElementById("tasca-id")
+const tascaTitol = document.getElementById("titol")
+const tascaDescripcio = document.getElementById("descripcio")
+const tascaPrioritat = document.getElementById("prioritat")
+const tascaDataVenciment = document.getElementById("data-venciment")
+const botonGuardar = document.getElementById("btn-guardar")
+const columnaPerfer = document.getElementById("tasques-perFer");
+const columnaEncurs = document.getElementById("tasques-enCurs");
+const columnaFet = document.getElementById("tasques-fet");
+
+/* Función que gestiona el Formulario */
+
+function gestionarFormulari(event) {
+    event.preventDefault();
+
+    const titol = tascaTitol.value.trim();
+    const descripcio = tascaDescripcio.value.trim();
+    const prioritat = tascaPrioritat.value;
+    const dataVenciment = tascaDataVenciment.value;
+
+    if (titol === "") {
+        alert("El titol és obligatori");
+        return
+    } 
+
+    const novaTasca = crearTasca(titol, descripcio, prioritat, dataVenciment);
+
+    tasques.push(novaTasca);
+    guardarTasques();
+    formulariTasca.reset();
+    renderTauler();
+}
+formulariTasca.addEventListener("submit", gestionarFormulari);
+
+/* Función que crea una targeta para añadirla al HTML */
+
+function crearTargetaTasca(tasca) {
+    const targeta = document.createElement("article");
+    targeta.classList.add("targeta-tasca");
+    const tascaTitol = document.createElement("h4");
+    tascaTitol.classList.add("titol-tasca");
+    tascaTitol.textContent = tasca.titol;
+    const tascaDescripcio = document.createElement("p");
+    tascaDescripcio.textContent = tasca.descripcio;
+    const dataTasca = document.createElement("p");
+    dataTasca.textContent = tasca.dataVenciment;
+    const tascaPrioritat = document.createElement("p");
+    tascaPrioritat.textContent = "Prioritat: " + tasca.prioritat;
+    if (tasca.prioritat === "alta") {
+        targeta.classList.add("prioritat-alta");
+    } else if ( tasca.prioritat === "mitjana") {
+        targeta.classList.add("prioritat-mitjana");
+    } else {
+        targeta.classList.add("prioritat-baixa");
+    }
+    targeta.appendChild(tascaTitol);
+    targeta.appendChild(tascaDescripcio);
+    targeta.appendChild(dataTasca);
+    targeta.appendChild(tascaPrioritat);
+    return targeta;
+
+}
+/* Función que escribe en las columnas las tareas que coinciden */
+
+function renderTauler(){
+    columnaEncurs.innerHTML = "";
+    columnaPerfer.innerHTML = "";
+    columnaFet.innerHTML= "";
+    tasques.forEach(tasca => {
+        let targetaTasca = crearTargetaTasca(tasca)
+        if (tasca.estat === "perFer") {
+            columnaPerfer.appendChild(targetaTasca)
+        } else if (tasca.estat === "enCurs") {
+            columnaEncurs.appendChild(targetaTasca)
+        } else {
+            columnaFet.appendChild(targetaTasca);
+        }
+    })
+
 }
 
 pruebaApp();
